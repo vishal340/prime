885d95f3c489b6c0bb12e749394542b67ad4d85f
vishal <vishal340@github.com>
2023-12-08 07:30:32 +0530

    makefile added and set_approach.cpp finished, also deleted input and output

---
+++ makefile
@@ -0,0 +1,4 @@
+# here prec is for precision of output
+set:
+	g++ -O3 set_approach.cpp -lgmpxx -lgmp -L/home/vishal340/vcpkg/packages/gmp_x64-linux/lib
+	./a.out input.txt output.txt $(prec)
--- set_approach.cpp
+++ set_approach.cpp
@@ -1,37 +1,27 @@
+// Copyright (C) 2023 All rights reserved.
+// Author        : vishal tripathy
+// Email         : vishaltripathy54@gmail.com
+
 #include <algorithm>
+#include <cstdint>
 #include <fstream>
+#include <gmpxx.h>
+#include <iomanip>
 #include <iostream>
 #include <random>
 #include <vector>
 
-void func(unsigned int *acc, unsigned int *prime,
-          const std::vector<unsigned int> &dist, unsigned int *ret, int rank,
-          int ones, int number) {
-  unsigned int total = 1, phi = 1;
-  for (int i = 0; i < number; i++) {
-    total *= prime[i];
-    phi *= prime[i] - 1;
-  }
-  for (unsigned int i = 1; i <= phi; i++) {
-    bool next = false;
-    for (int j = 0; j < number; j++) {
-      if (i % prime[j] == 0) {
-        next = true;
-        break;
-      }
-    }
-    if (next)
-      continue;
-    std::vector<unsigned int> temp;
-    std::copy(dist.begin(), dist.end(), temp);
-    for (int j = 1; j < ones; j++)
-      temp[j] = ((temp[j] - temp[0]) * i + temp[j]) % total;
-    std::sort(temp.begin(), temp.end());
+void permute(std::vector<mpz_class> *comb, int ones) {
+  int t = ones * (ones - 1);
+  t /= 2;
+  comb->push_back(t);
+  for (int i = 3; i <= ones; i++) {
+    auto t1 = (ones - i + 1) * t;
+    t1 /= i;
+    t = t1;
+    comb->push_back(t);
   }
 }
-
-// TODO: do permutation calcualtion and save it in 2D array
-void permute() {}
 
 int main(int argc, char *argv[]) {
   std::ifstream in(argv[1]);
@@ -42,13 +32,21 @@ int main(int argc, char *argv[]) {
     int number;
     in >> number;
     std::vector<unsigned int> prime(number);
-    uint32_t total = 1;
+    uint32_t total = 1, phi = 1;
     for (int i = 0; i < number; i++) {
       in >> prime[i];
       total *= prime[i];
+      phi *= prime[i] - 1;
     }
-    int ones, rank;
-    in >> ones >> rank;
+    int ones, rank, x;
+    in >> ones >> rank >> x;
+
+    std::vector<std::vector<mpz_class>> comb(ones - 1);
+
+    for (int i = 0; i < ones - 1; i++) {
+      permute(&comb[i], i + 2);
+    }
+
     std::vector<std::vector<unsigned int>> basis(
         rank, std::vector<unsigned int>(ones, 0));
     for (int i = 0; i < rank; i++) {
@@ -63,14 +61,65 @@ int main(int argc, char *argv[]) {
           temp[j] = t;
       }
     }
-    std::vector<std::vector<unsigned int>> shortest(
-        rank, std::vector<unsigned int>(ones, 0));
-    std::vector<std::vector<unsigned int>> acc(
-        ones - 1, std::vector<unsigned int>(total - 1, 0));
+    std::vector<mpz_class> acc(ones - 1, 0);
+    auto func = [phi, total, number, ones, prime, x, comb,
+                 &acc](const std::vector<unsigned int> &dist) {
+      for (unsigned int i = 1; i <= phi; i++) {
+        bool next = false;
+        for (int j = 0; j < number; j++) {
+          if (i % prime[j] == 0) {
+            next = true;
+            break;
+          }
+        }
+        if (next)
+          continue;
+        std::vector<unsigned int> temp(dist);
+        for (int j = 1; j < ones; j++)
+          temp[j] = ((temp[j] - temp[0]) * i + temp[j]) % total;
+        std::sort(temp.begin(), temp.end());
+        for (int t = 0; t < total; t++) {
+          int ret;
+          if (t + x > t) {
+            ret = std::count_if(temp.begin(), temp.end(), [t, x](auto a) {
+              if (a >= t && a < t + x)
+                return true;
+            });
+          } else {
+            ret = std::count_if(temp.begin(), temp.end(), [t, x](auto a) {
+              if (a >= t || a < t + x)
+                return true;
+            });
+          }
+          for (int k = 2; k <= ret; k++) {
+            acc[k - 2] += comb[ret - 2][k - 2];
+          }
+        }
+      }
+    };
     for (int i = 0; i < rank; i++) {
-      func(&acc[0][0], &prime[0], basis[i], &shortest[i][0], rank, ones,
-           number);
+      func(basis[i]);
     }
+    out << it << '\n';
+    mpz_class normalize = x * (x - 1);
+    normalize /= 2;
+    mpf_class compare = (mpf_class)(2 * phi) / (total - 1);
+    int inc = 2;
+    uint64_t prod = (uint64_t)total * phi * rank;
+    for (auto a : acc) {
+      if (a != 0) {
+        mpf_class temp = (mpf_class)a / normalize;
+        temp /= prod;
+        out << std::setprecision(atoi(argv[3])) << temp << ' ' << compare
+            << '\n';
+        normalize *= x - inc;
+        compare *= (mpf_class)(inc + 1) / (total - inc);
+        inc++;
+        normalize /= inc;
+      } else
+        break;
+    }
+    out << '\n';
   }
   return 0;
 }
